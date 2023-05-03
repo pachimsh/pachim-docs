28a23261958dcc3b11ef071d16a403bc4d9ff3ed
Hesam Mousavi <hesammoousavi@gmail.com>
2023-05-03 08:10:50 +0330

    add document page to right to left

---
+++ src/pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html dir="rtl">
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
